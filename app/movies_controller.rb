04a28d93e2a75fe179ef90a43b79966357905497
David Kuruvilla <dapawn@gmail.com>
2018-01-11 23:34:09 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,10 +24,10 @@ end
 
 def can_be_created_in_a_block
   Movie.create do |m|
-    m.title = "Home Alone",
-    m.release_date = 1990,
-    m.director = "Unknown",
-    m.lead = "The blond haired kid",
+    m.title = "Home Alone"
+    m.release_date = 1990
+    m.director = "Unknown"
+    m.lead = "The blond haired kid"
     m.in_theaters = false
   end
 end
